5faa57cbde704be1fc2b93f0f23c362a49ade7b8
Mark Strempel <mark.strempel@codecool.com>
2023-07-03 14:10:11 +0200

    Basic skeleton for the server.

---
+++ server/src/main/java/at/codecool/humanoraiserver/HumanOrAiServerApplication.java
@@ -0,0 +1,13 @@
+package at.codecool.humanoraiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanOrAiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanOrAiServerApplication.class, args);
+	}
+
+}
