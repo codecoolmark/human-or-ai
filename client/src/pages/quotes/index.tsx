01fa35cacea971583764f156ae2271c40939ace2
Mark Strempel <mark.strempel@codecool.com>
2023-07-04 13:12:53 +0200

    Show extra information about quote on the quotes page.

--- client/src/pages/quotes/index.tsx
+++ client/src/pages/quotes/index.tsx
@@ -1,6 +1,12 @@
 import { getQuotes} from "../../Api.tsx";
 import {useEffect, useState} from "react";
 
+function formateDateTime(instant: string) {
+    return new Intl.DateTimeFormat(navigator.language, {
+        dateStyle: "full", timeStyle: "medium"
+    }).format(new Date(instant))
+}
+
 export default function Quotes() {
     const [quotes, setQuotes] = useState([])
 
@@ -11,9 +17,24 @@ export default function Quotes() {
     return (
         <main>
             <h1>Quotes</h1>
-            <ul>
-                {quotes.map(quote => <li>{quote.text}</li>)}
-            </ul>
+            <figure>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Quote</th>
+                            <th>Human or AI</th>
+                            <th>Expires</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {quotes.map(quote => <tr>
+                        <td>{quote.text}</td>
+                        <td>{quote.real ? "Human" : "AI"}</td>
+                        <td>{formateDateTime(quote.expires)}</td>
+                    </tr>)}
+                    </tbody>
+                </table>
+            </figure>
         </main>
     );
 }
