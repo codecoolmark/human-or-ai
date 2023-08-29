eff53f7105b04bc1350c17a6b3bcc0aed84fb963
Mark Strempel <mark.strempel@codecool.com>
2023-08-29 09:13:48 +0200

    Improve error handling on the /vote page.

--- server/src/main/java/at/codecool/humanoraiserver/controller/QuoteController.java
+++ server/src/main/java/at/codecool/humanoraiserver/controller/QuoteController.java
@@ -2,6 +2,8 @@ package at.codecool.humanoraiserver.controller;
 
 import at.codecool.humanoraiserver.repositories.UsersRepository;
 import at.codecool.humanoraiserver.services.QuotesService;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +22,13 @@ public class QuoteController {
     }
 
     @PostMapping ("/quote")
-    public PostQuoteResponse postQuote(Authentication authentication) {
+    public ResponseEntity<PostQuoteResponse> postQuote(Authentication authentication) {
         var user = usersRepository.findByEmail(authentication.getName());
         var quoteOpt = quotesService.nextQuoteForUser(user.orElseThrow());
         if (quoteOpt.isEmpty()) {
-            return null;
+            return new ResponseEntity<>(null, HttpStatusCode.valueOf(404));
         }
 
-        return new PostQuoteResponse(quoteOpt.get());
+        return new ResponseEntity<>(new PostQuoteResponse(quoteOpt.get()), HttpStatusCode.valueOf(200));
     }
 }
